c44d72737c9a714950733da55a2d1c435e430f37
Dymas <dymassousa@gmail.com>
2024-03-23 18:49:26 -0300

    feat(frontend): add NavBar.vue

---
+++ app/trip_calc_app/src/components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav class="nav"></nav>
+</template>
+
+<style scoped>
+.nav {
+  background-color: #ffffff;
+  box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
+  height: 70px;
+  width: 100%;
+}
+</style>
