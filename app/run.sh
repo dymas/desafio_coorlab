3d206d367db77465c601eaadec47e9acab3a0441
Dymas <dymassousa@gmail.com>
2024-03-23 18:51:19 -0300

    build: add commands to run.sh

--- app/run.sh
+++ app/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo "Implemente aqui o script para executar a sua solução"
+pip3 install -r ./backend/requirements.txt
+npm install --prefix ./trip_calc_app
+npm run dev --prefix ./trip_calc_app | uvicorn --port 3000 --app-dir ./backend main:app
