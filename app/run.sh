0b4f9fefa2596f7a6862fcdc0855e120bbe002d5
Dymas <dymassousa@gmail.com>
2024-03-23 19:40:29 -0300

    fix: correction in commands in run.sh

--- app/run.sh
+++ app/run.sh
@@ -2,4 +2,4 @@
 
 pip3 install -r ./backend/requirements.txt
 npm install --prefix ./trip_calc_app
-npm run dev --prefix ./trip_calc_app | uvicorn --port 3000 --app-dir ./backend main:app
+python3 -m uvicorn --port 3000 --app-dir ./backend main:app | wait | npm run dev --prefix ./trip_calc_app
